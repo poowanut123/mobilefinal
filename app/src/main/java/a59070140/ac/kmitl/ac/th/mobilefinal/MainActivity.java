e8fb8ba87af43b1d78a458eade93798ea714a8ab
poowanut123 <59070140@kmitl.ac.th>
2018-12-11 09:45:30 +0700

    start upload

---
+++ app/src/main/java/a59070140/ac/kmitl/ac/th/mobilefinal/MainActivity.java
@@ -0,0 +1,13 @@
+package a59070140.ac.kmitl.ac.th.mobilefinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
