0247c9c9fa38d5e5ee46cfa405bd23bcc4db8ca1
poowanut123 <59070140@kmitl.ac.th>
2018-12-11 12:43:03 +0700

    a

--- app/src/main/java/a59070140/ac/kmitl/ac/th/mobilefinal/MainActivity.java
+++ app/src/main/java/a59070140/ac/kmitl/ac/th/mobilefinal/MainActivity.java
@@ -1,13 +1,34 @@
 package a59070140.ac.kmitl.ac.th.mobilefinal;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_fragment);
+        setContentView(R.layout.login_fragment);
+        Button login_btn = (Button) findViewById(R.id.login_btn);
+
+
+        TextView register_btn = (TextView) findViewById(R.id.register_btn);
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Register_fragment.class));
+            }
+        });
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Home_fragment.class));
+            }
+        });
     }
 }
